472979dfb86497f7d1a9e6c2b6fefcbf3035b127
Darion Welch <darion@getplatypi.com>
2015-01-11 09:52:06 -0600

    added mongolab path and switch to prod env

--- server/config/config.ts
+++ server/config/config.ts
@@ -1,7 +1,7 @@
 /// <reference path="../typings/tsd.d.ts" />
 import path = require('path');
 
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
 
 var cfg = require('./env/' + process.env.NODE_ENV + '.json') || {};
 
